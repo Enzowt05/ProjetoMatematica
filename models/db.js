a95be4aa71ed525fa5ecf9e5a58ced7a85f87215
Enzowt <130841599+Enzowt05@users.noreply.github.com>
2024-05-02 19:38:27 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 //Conexão com o banco de dados
-const sequelize = new Sequelize('db_matematica', 'root', 'root', {
-    host: "localhost",
-    port: "3306",
+const sequelize = new Sequelize('db_matematica', 'db_matematica_user', 'uBzmxXvonWf3WAFiyaboVKIxRJBacmZc', {
+    host: "dpg-coq1aon79t8c738841rg-a",
+    port: "5432",
     dialect: 'mysql'
 });
 //Vamos exportar as variáveis
